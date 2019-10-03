4a31c42883d16419eee0e1b760ad438974835b04
mahfudz aji <mahfudz.aji16@gmail.com>
2019-10-03 12:33:47 +0700

    just python

--- get_email_whois.py
+++ get_email_whois.py
@@ -4,4 +4,20 @@ f=open("list_domain","r")
 domain_name=f.read().splitlines()
 
 for i in domain_name:
-  print(whois.whois(i).emails)
+  emails=''
+  email_list=whois.whois(i).emails
+  #check if emails is list or not
+  is_list=isinstance(email_list, list)
+
+  #if list then doing for loop and make it as string
+  #if not then just print as is
+  #store it into variable then embed that variable with domain name
+  if is_list:
+      for x in email_list:
+          emails+=x+", "
+  else:
+      emails=email_list
+  
+  print(i+" : "+emails.rstrip(','));
+
+  
