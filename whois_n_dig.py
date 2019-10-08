230b1e4eb7e36ea6853011d76f44ab56f40ae6bc
mahfudz aji <mahfudz.aji16@gmail.com>
2019-10-08 16:57:48 +0700

    adding feature to check whois data of domain

--- whois_n_dig.py
+++ whois_n_dig.py
@@ -1,8 +1,24 @@
 import os
 import subprocess
+import whois
+
+print("Please input domain name:")
+domain=input()
+print("Please input record type eg A, NS, MX")
+record_type=input()
+
+whois_domain=whois.whois(domain)
+
+print("\n==================")
+print("Domain: "+whois_domain.domain_name)
+print("Registrar: "+whois_domain.registrar)
+print("Creation date: "+str(whois_domain.creation_date))
+print("Expiration date: "+str(whois_domain.expiration_date))
+print("status: "+whois_domain.status)
+print("\n==================")
 
 line_found=False
-dig_domain=os.system("dig detik.com > temp")
+dig_domain=os.system("dig "+domain+" "+record_type+" > temp")
 get_answer_line=int(subprocess.check_output("cat temp| grep -n 'ANSWER SECTION' | cut -d: -f1", shell=True).decode("utf-8"))
 
 if get_answer_line!=0:
